a4898e7633f4aaea5234c70dae07e91bd8061ccd
Maxtadont <tusaeff@gmail.com>
2020-09-30 20:49:39 +0400

    feat: add my own min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+const { expect } = require("chai");
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {  
+  if (!array || array.length === 0) 
+    return 0;
+
+  let fmin = array[0];
+  for (let el of array)
+    if (fmin > el)
+      fmin = el;
+  return fmin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) 
+    return 0;
+    
+  let fmax = array[0];
+  for (let el of array)
+    if (fmax < el)
+      fmax = el;
+  return fmax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) 
+    return 0;
+  
+    let favg = 0;
+    for (let el of array)
+      favg += el;
+    return favg/array.length;
 }
